75a13884aeb6039cafcb31bae582a5eea58fff27
made junes <made.junes@gmail.com>
2023-06-01 22:47:50 +0800

    define Product type and create cart store

---
+++ src/store/useCartStore.ts
@@ -0,0 +1,47 @@
+import { Product } from "@/types"
+import { create } from "zustand"
+
+type State = {
+  cart: Product[]
+  totalItems: number
+  totalPrice: number
+}
+
+type Actions = {
+  addToCart: (Item: Product) => void
+  removeFromCart: (Item: Product) => void
+}
+
+const INITIAL_STATE: State = {
+  cart: [],
+  totalItems: 0,
+  totalPrice: 0
+}
+
+export const useCartStore = create<State & Actions>((set, get) => ({
+  cart: INITIAL_STATE.cart,
+  totalItems: INITIAL_STATE.totalItems,
+  totalPrice: INITIAL_STATE.totalPrice,
+  addToCart: (product: Product) => {
+    const cart = get().cart // get cart dari state
+    const cartItem = cart.find(item => item.id === product.id) // cari id-nya buat cek apakah sudah ada di cart atau belum
+
+    if (cartItem) {
+      const updatedCart = cart.map(item => 
+        item.id == product.id ? {...item, quantity: (item.quantity as number) + 1} : item
+      )
+      set(state => ({
+        cart: updatedCart,
+        totalItems: state.totalItems + 1,
+        totalPrice: state.totalPrice + product.price
+      }))
+    }
+  },
+  removeFromCart(product: Product) {
+    set(state => ({
+      cart: state.cart.filter(item => item.id !== product.id), // hmm filter cart dengan exclude product yg mau di-remove, bisa juga sih caranya begini
+      totalItems: state.totalItems - 1,
+      totalPrice: state.totalPrice - product.price
+    }))
+  },
+}))
