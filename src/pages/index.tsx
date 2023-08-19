f8b471161406aebcc8587209d72c7d6230e2deab
made junes <made.junes@gmail.com>
2023-08-19 17:03:28 +0800

    use products store on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,38 @@
-import ProductList from '@/components/products/ProductList'
-import { products } from '@/data/db-dummy'
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
-import Header from '@/components/ui/Header'
-import Drawer from '@/components/ui/Drawer'
-import Cart from '@/components/minicart/Cart'
+import ProductList from "@/components/products/ProductList";
+import Header from "@/components/ui/Header";
+import Drawer from "@/components/ui/Drawer";
+import Cart from "@/components/minicart/Cart";
+
+import { useProductsStore } from "@/stores/useProductStore";
 
 export default function Home() {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
-  
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const { isLoading, products, fetchData } = useProductsStore();
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   const handleCartIconClick = () => {
-    setIsDrawerOpen(!isDrawerOpen)
-  }
+    setIsDrawerOpen(!isDrawerOpen);
+  };
 
   return (
     <>
-    <Header onCartIconClick={handleCartIconClick} />
-    <Drawer isOpen={isDrawerOpen} onCartIconClick={handleCartIconClick}>
-      <Cart />
-    </Drawer>
-    <main className='container mx-auto md:w-10/12 py-8 px-4'>
-      <ProductList products={products} />
-    </main>
+      <Header onCartIconClick={handleCartIconClick} />
+      <Drawer isOpen={isDrawerOpen} onCartIconClick={handleCartIconClick}>
+        <Cart />
+      </Drawer>
+      <main className="container mx-auto md:w-10/12 py-8 px-4">
+        {isLoading ? (
+          <div className="text-center text-lg">Loading...</div>
+        ) : (
+          <ProductList products={products} />
+        )}
+      </main>
     </>
-  )
+  );
 }
